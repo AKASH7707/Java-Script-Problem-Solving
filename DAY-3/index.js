9e216be9a14308eae4c60127848db7bd33f05d09
AKASH7707 <akashvs7707@gmail.com>
2026-03-24 13:28:42 +0530

    ternary and truthy  operator

---
+++ DAY-3/index.js
@@ -0,0 +1,18 @@
+// let a = 1;
+// console.log(a++)
+// console.log(++a)
+// console.log(a)
+
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+let c
+console.log()
---
+++ DAY-3/ternaryOperator.js
@@ -0,0 +1,27 @@
+// let age = 18;
+// age>18 ? console.log("eligible") : console.log("not eligible");
+
+// w.a.p. to check odd or even
+let num = 10;
+num%2 == 0 ? console.log("even number") : console.log("odd number");
+
+
+// w.a.p. to check positve or negative
+let tempNum = 0;
+tempNum>0 ? console.log("number is positive") : console.log("number is negative")
+
+
+// w.a.p. to check vote eligblity according to age
+let age = 15;
+age>=18 ? console.log("eligible to vote") : console.log("not eligible to vote");
+
+
+// w.a.p. to find largest of two numbers
+let num_1 = 60;
+let num_2 = 20;
+num_1 > num_2 ? console.log(num_1 + " is greater"): console.log(num_2 + " is greater")
+
+let numm = 0;
+
+(numm > 0) ? console.log("positive") : (numm < 0) ? console.log("negative") : console.log("zero");
+
